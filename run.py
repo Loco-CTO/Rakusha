b05d95c2915fcdbc489ea882a7cdd4df577c2e64
Poco <61045140+Loco-CTO@users.noreply.github.com>
2025-01-28 00:05:09 +0000

    feat: add admin account creation functionality on production run

--- run.py
+++ run.py
@@ -1,6 +1,14 @@
 from waitress import serve
 
+from models.user import User, create_account
 from app import app
+
+def create_admin_account():
+    if User.get_user_count() == 0:
+        create_account("admin", "1234", is_admin=True)
+        print("Admin account created.")
+    else:
+        print("Admin account already exists.")
 
 if __name__ == "__main__":
     serve(app, host="0.0.0.0", port=25621)
