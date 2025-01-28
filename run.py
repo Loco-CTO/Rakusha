747f3f031ed4af5a346bae3f18a077aba02e9691
Poco <61045140+Loco-CTO@users.noreply.github.com>
2025-01-28 00:14:08 +0000

    feat: replace print statements with logging in create_admin_account function

--- run.py
+++ run.py
@@ -4,12 +4,22 @@ import logging
 from models.user import User, create_account
 from app import app
 
+
 def create_admin_account():
     if User.get_user_count() == 0:
         create_account("admin", "1234", is_admin=True)
-        print("Admin account created.")
+        logging.debug("Admin account created.")
     else:
-        print("Admin account already exists.")
+        logging.debug("Admin account already exists.")
+
+
+logging.basicConfig(level=logging.DEBUG, format="%(asctime)s %(message)s")
+
 
 if __name__ == "__main__":
-    serve(app, host="0.0.0.0", port=25621)
+    create_admin_account()
+    serve(
+        app,
+        host="0.0.0.0",
+        port=25621,
+    )
